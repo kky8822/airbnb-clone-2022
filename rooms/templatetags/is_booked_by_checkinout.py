7ee8adf2d628201c8970312401bf0107817c5b5b
kky8822 <kky8822@gmail.com>
2022-05-24 10:29:56 +0900

    220524_is_booked_by_checkinout_tag

--- rooms/templatetags/is_booked_by_checkinout.py
+++ rooms/templatetags/is_booked_by_checkinout.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 from django import template
 from reservations import models as reservation_models
 
@@ -11,14 +12,21 @@ def is_booked_by_checkinout(room, day):
         return
     try:
         date = datetime.datetime(year=day.year, month=day.month, day=day.day)
-        reservation = reservation_models.Reservation.objects.get(
-            day=date, reservation__room=room
-        )
-        check_in = datetime.datetime(room.check_in)
-        check_out = datetime.datetime(room.check_out)
-        if date >= check_in and date <= check_out:
-            return True
-        else:
-            return False
+        reservations = reservation_models.Reservation.objects.filter(room=room)
+        for reservation in reservations:
+
+            check_in = datetime.datetime.combine(
+                reservation.check_in, datetime.datetime.min.time()
+            )
+            check_out = datetime.datetime.combine(
+                reservation.check_out, datetime.datetime.min.time()
+            )
+            print(check_in, check_out, date, date >= check_in and date <= check_out)
+
+            if date >= check_in and date <= check_out:
+                return True
+
+        return False
+
     except reservation_models.BookedDay.DoesNotExist:
         return False
